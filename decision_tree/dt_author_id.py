f0155986ba4b0d248e951c79b1285bd7d05aa562
Jiadai Zhao <jiadaizhao@gmail.com>
2016-05-08 21:30:38 -0500

    Decision Trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+print "Number of feature is ", len(features_train[0])
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+print "accuracy is", accuracy_score(pred, labels_test)
 
 #########################################################
 

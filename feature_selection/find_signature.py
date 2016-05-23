6a9c1956d13a39c35d73f0de460a2ab407897a2d
Jiadai Zhao <jiadaizhao@gmail.com>
2016-05-22 23:01:43 -0500

    Feature Selection

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -38,6 +38,18 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
+print "accuracy is", accuracy_score(pred, labels_test)
 
+index = 0
+for i, feature in enumerate(clf.feature_importances_):
+    if feature > 0.2:
+        index = i
+        print i, feature
 
+print vectorizer.get_feature_names()[index]
